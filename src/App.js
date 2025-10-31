495d69a8065e122128614fe4752a7a7c41492a4e
Abd-2004 <abdullahmostafahamad@gmail.com>
2025-10-31 17:39:47 +0200

    Created environment variable for API URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const [isLogin, setIsLogin] = useState(true);
-  const API_URL = 'http://localhost:3000';
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
